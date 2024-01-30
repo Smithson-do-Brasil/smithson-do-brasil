5a2e378b750533ad90951dae46595f55680b2017
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-01-30 16:57:21 -0300

    style: update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,32 +3,27 @@ import { Link } from 'react-router-dom'
 
 import { NavLink } from './nav-link'
 import { ThemeToggle } from './theme/theme-toggle'
-import { Separator } from './ui/separator'
 
 export function Header() {
   return (
-    <div className="sticky top-0 z-50 border-b">
-      <div className="flex h-16 items-center gap-6 px-6">
-        <Link to="/" className="flex items-center gap-2">
-          <Computer className="text-md font-bold text-foreground" />
-          <h1 className="text-md font-bold text-foreground">
-            Smithson do Brasil
-          </h1>
-        </Link>
+    <>
+      <div className="sticky top-0 z-50">
+        <div className="flex h-16 items-center justify-between gap-6 px-6">
+          <Link to="/">
+            <Computer className="text-md font-bold text-foreground" />
+          </Link>
 
-        <Separator orientation="vertical" className="h-6" />
+          <nav className="flex items-center space-x-4 lg:space-x-6">
+            <NavLink to="/about-us">Sobre nós</NavLink>
+            <NavLink to="/services">Nossos serviços</NavLink>
+            <NavLink to="/contact">Contato</NavLink>
+          </nav>
 
-        <nav className="flex items-center space-x-4 lg:space-x-6">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about-us">Sobre nós</NavLink>
-          <NavLink to="/services">Nossos serviços</NavLink>
-          <NavLink to="/contact">Contato</NavLink>
-        </nav>
-
-        <div className="ml-auto flex items-center">
-          <ThemeToggle />
+          <div className="flex items-center">
+            <ThemeToggle />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
