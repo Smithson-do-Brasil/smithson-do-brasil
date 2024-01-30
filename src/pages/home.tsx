fb369ca52716b57c78e25dc7d7cb978c841edaf0
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-01-30 15:48:17 -0300

    feat: adding shadcn ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,14 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+
 export function Home() {
   return (
     <>
       <Helmet title="Home" />
-      <h1>Home</h1>
+      <div className="flex h-screen items-center justify-center">
+        <Button>Enviar</Button>
+      </div>
     </>
   )
 }
