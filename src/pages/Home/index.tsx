f443aa2b929933e9fb5e9bf29c47b49f22c2be3c
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-01-31 17:35:56 -0300

    refactor: moving section to homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -46,6 +46,17 @@ export function Home() {
           <img src="image.svg" alt="" className="w-full" />
         </section>
       </div>
+      <div className="flex flex-col items-center gap-8 bg-[#4C6FF7] p-16">
+        <h1 className="text-center text-2xl font-medium text-zinc-50 lg:text-4xl">
+          Acelere seu negócio hoje
+        </h1>
+
+        <Link to="/contact">
+          <Button className="bg-zinc-50 text-lg text-zinc-950 lg:text-xl">
+            Entre em contato
+          </Button>
+        </Link>
+      </div>
     </>
   )
 }
