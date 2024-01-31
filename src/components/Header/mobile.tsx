128daacd1e95040abfa3b43b16ed770699eb0fac
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-01-31 15:48:01 -0300

    style: update space in header

--- src/components/Header/mobile.tsx
+++ src/components/Header/mobile.tsx
@@ -22,8 +22,8 @@ export function Mobile() {
             <Menu className="text-zinc-950 dark:text-zinc-50" />
           </Button>
         </DrawerTrigger>
-        <DrawerContent className="flex h-full w-[40%] flex-col gap-5">
-          <DrawerHeader className="flex w-full items-center justify-start gap-2">
+        <DrawerContent className="flex h-full w-[50%] flex-col">
+          <DrawerHeader className="flex w-full items-center justify-start gap-2 p-4">
             <a href="https://facebook.com.br" target="_blank" rel="noreferrer">
               <Facebook />
             </a>
@@ -35,13 +35,13 @@ export function Mobile() {
           <Separator className="w-full bg-zinc-950 dark:bg-zinc-50" />
 
           <nav className="flex flex-col space-y-4 p-4 lg:space-y-6">
-            <NavLink to="/about-us" className="text-lg font-medium">
+            <NavLink to="/about-us" className="text-sm font-medium">
               Sobre nós
             </NavLink>
-            <NavLink to="/services" className="text-lg font-medium">
+            <NavLink to="/services" className="text-sm font-medium">
               Nossos serviços
             </NavLink>
-            <NavLink to="/contact" className="text-lg font-medium">
+            <NavLink to="/contact" className="text-sm font-medium">
               Contato
             </NavLink>
           </nav>
