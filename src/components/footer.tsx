4e49df3cf2a0ba35173ad8b4cf82197b57375b09
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-02-01 19:19:51 -0300

    feat: adjusting footer font size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="flex items-center justify-center p-3 text-center">
+    <footer className="flex items-center justify-center p-3 text-center text-xs md:text-base">
       &copy; {new Date().getFullYear()}, Smithson do Brasil - CNPJ
       00.460.978/0001-36. Todos os direitos reservados.
     </footer>
