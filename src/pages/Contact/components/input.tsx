1ce670e605035e00962ee361d0924a2cac12e284
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-02-29 17:26:28 -0300

    feat: update favicon and background for contact in light mode

--- src/pages/Contact/components/input.tsx
+++ src/pages/Contact/components/input.tsx
@@ -40,7 +40,7 @@ export function Input({
       {asTextarea ? (
         <Textarea
           id={id}
-          className="rounded-none border-x-0 border-b-2 border-t-0 border-zinc-950 bg-zinc-200 pl-0 font-medium text-foreground focus-visible:border-b-[#4C6FF7] focus-visible:shadow-transparent focus-visible:ring-transparent focus-visible:ring-offset-0 dark:border-b-zinc-50 dark:bg-zinc-800"
+          className="rounded-none border-x-0 border-b-2 border-t-0 border-zinc-950 bg-transparent pl-0 font-medium text-foreground focus-visible:border-b-[#4C6FF7] focus-visible:shadow-transparent focus-visible:ring-transparent focus-visible:ring-offset-0 dark:border-b-zinc-50 dark:bg-zinc-800"
           {...register(id)}
           {...rest}
         />
@@ -48,7 +48,7 @@ export function Input({
         <ShadcnInput
           id={id}
           type={type}
-          className="rounded-none border-x-0 border-b-2 border-t-0 border-zinc-950 bg-zinc-200 pl-0 font-medium text-foreground focus-visible:border-b-[#4C6FF7] focus-visible:shadow-transparent focus-visible:ring-transparent focus-visible:ring-offset-0 dark:border-b-zinc-50 dark:bg-zinc-800"
+          className="rounded-none border-x-0 border-b-2 border-t-0 border-zinc-950 bg-transparent pl-0 font-medium text-foreground focus-visible:border-b-[#4C6FF7] focus-visible:shadow-transparent focus-visible:ring-transparent focus-visible:ring-offset-0 dark:border-b-zinc-50 dark:bg-zinc-800"
           {...register(id)}
           {...rest}
         />
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -18,6 +18,13 @@ export function Contact() {
 
         <Form />
       </div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3674.883365723487!2d-43.3972182882957!3d-22.917672438175153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9bd8f59f2840d1%3A0x25077c1eae0c3c88!2sSmithson%20do%20Brasil%20Inform%C3%A1tica!5e0!3m2!1spt-BR!2sbr!4v1709237917909!5m2!1spt-BR!2sbr"
+        width="100%"
+        height="450"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </>
   )
 }
