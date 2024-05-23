835ff585346f14687c30827864709fdc9e53377f
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-05-23 15:34:01 -0300

    style: update badge color in partners

--- src/pages/Home/components/partners.tsx
+++ src/pages/Home/components/partners.tsx
@@ -98,7 +98,7 @@ export function Partners() {
       className="my-20 flex flex-col items-center justify-center gap-8 lg:gap-20"
     >
       <div className="container space-y-8 text-center">
-        <Badge className="border-2 border-[#2364AA] bg-zinc-50 px-5 py-2 text-2xl font-medium text-[#2364AA] hover:bg-zinc-50">
+        <Badge className="border-2 border-[#2364AA] bg-muted px-5 py-2 text-2xl font-medium text-[#2364AA] hover:bg-muted">
           Nossos parceiros
         </Badge>
 
