ecdd700862151287b4093951e5ccbb3af2f58b6d
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-06-14 11:08:08 -0300

    feat: extend a new color

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { Layout } from './pages/_layout'
 import { NotFound } from './pages/404'
 import { AboutUs } from './pages/AboutUs'
 import { Contact } from './pages/Contact'
 import { Home } from './pages/Home'
-import { Layout } from './pages/layout'
 import { Policies } from './pages/Policies'
 import { Services } from './pages/Services'
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,9 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        teal: {
+          200: '#CCFBF1',
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
