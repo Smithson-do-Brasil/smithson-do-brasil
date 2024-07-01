6c05fb83ba3e8cfda2021120cfebcf911c8125ef
Felipe Bastos <felipe.bastos.dev@gmail.com>
2024-07-01 12:45:06 -0300

    style: update logo colorr

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,17 +11,17 @@ export function Logo() {
       <path
         d="M2.52416 11.5461C1.13011 9.55958 1.61036 6.81911 3.59685 5.42506L10.4636 0.606201C11.8576 2.59268 11.3774 5.33315 9.39087 6.72721L2.52416 11.5461Z"
         fill="#0C0A09"
-        className="fill-zinc-950 dark:fill-teal-200"
+        className="fill-zinc-950 dark:fill-zinc-50"
       />
       <path
         d="M2.75391 11.8733L9.62061 7.05444C11.6071 5.66038 14.3476 6.14064 15.7416 8.12712L8.87492 12.946C6.88843 14.34 4.14796 13.8598 2.75391 11.8733Z"
         fill="#0C0A09"
-        className="fill-zinc-950 dark:fill-teal-200"
+        className="fill-zinc-950 dark:fill-zinc-50"
       />
       <path
         d="M8.03149 19.3937C6.63743 17.4072 7.11769 14.6668 9.10417 13.2727L15.9709 8.45386C17.3649 10.4403 16.8847 13.1808 14.8982 14.5749L8.03149 19.3937Z"
         fill="#0C0A09"
-        className="fill-zinc-950 dark:fill-teal-200"
+        className="fill-zinc-950 dark:fill-zinc-50"
       />
     </svg>
   )
